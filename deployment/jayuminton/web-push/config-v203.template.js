906437013136a26243a474e35f731402c5444f99
pianopp001-cpu <pianopp001@gmail.com>
2026-08-07 00:23:35 +0900

    Add PWA deployment config template

---
+++ deployment/jayuminton/web-push/config-v203.template.js
@@ -0,0 +1,15 @@
+window.JAYUMINTON_PUSH_SETUP_CONFIG = Object.freeze({
+  build: '203',
+  relayUrl: '__RELAY_URL__',
+  memberPageUrl: '__MEMBER_PAGE_URL__',
+  appUrl: 'https://jayuminton-push.web.app/',
+  vapidKey: 'BNUQPnrOB2AKL7vkrpWQCqtCUyeQN9OInABU5eWBXpF1vGNTuW5ozOV5cdSBNj8d_y0Yc0UB8dolRz2xPLkZ0Rw',
+  firebase: {
+    apiKey: 'AIzaSyCS8MJsLHfjsiaQymEyEn-qqp_05WSW1cI',
+    authDomain: 'jayuminton-push.firebaseapp.com',
+    projectId: 'jayuminton-push',
+    storageBucket: 'jayuminton-push.firebasestorage.app',
+    messagingSenderId: '758697255400',
+    appId: '1:758697255400:web:7214800018c65b7827045d'
+  }
+});
